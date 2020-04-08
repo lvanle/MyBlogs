6b418674615ca708931e5ce1ce8106c8d6e5acd2
tonysmark <mjyuyu@qq.com>
2020-04-08 11:53:47 +0800

    num1

---
+++ src/components/BlogHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="blogheader">
+    <nav>
+      <ul>
+        <li>
+          <router-link to="/" exact>博客</router-link>
+          <router-link to="/add" exact>写博客</router-link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "blog-header"
+};
+</script>
+
+<style>
+ul {
+  list-style-type: none;
+  text-align: center;
+  margin: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #fff;
+  text-decoration: none;
+  padding: 12px;
+  border-radius: 5px;
+}
+nav {
+  background: crimson;
+  padding: 20px 0;
+  margin-bottom: 30px;
+}
+.router-link-active {
+  background: rgba(255, 255, 255, 0.8);
+  color: #444;
+}
+</style>
